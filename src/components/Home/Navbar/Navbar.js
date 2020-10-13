0f42954c2e3fb3452aa3e82a85d8dadea03a91db
Arefi6 <arefinhossain3@gmail.com>
2020-10-13 12:55:31 +0600

    Service and header added

--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -2,30 +2,30 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-<nav class="navbar navbar-expand-lg navbar-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-light">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link mr-5" href="#">Home</a>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item active">
+        <a className="nav-link mr-5" href="#">Home</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link mr-5" href="#">About</a>
+      <li className="nav-item">
+        <a className="nav-link mr-5" href="#">About</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link mr-5" href="#">Reviews</a>
+      <li className="nav-item">
+        <a className="nav-link mr-5" href="#">Reviews</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link mr-5 text-white" href="#">Dental Services</a>
+      <li className="nav-item">
+        <a className="nav-link mr-5 text-white" href="#">Dental Services</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link mr-5 text-white" href="#">Blog</a>
+      <li className="nav-item">
+        <a className="nav-link mr-5 text-white" href="#">Blog</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link mr-5 text-white" href="#">Contact Us</a>
+      <li className="nav-item">
+        <a className="nav-link mr-5 text-white" href="#">Contact Us</a>
       </li>
     </ul>
   </div>
